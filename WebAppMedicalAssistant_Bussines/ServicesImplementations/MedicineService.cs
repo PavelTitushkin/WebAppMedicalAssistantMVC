3c903738f3b1518cbf4be4d7bede5c4213e04c99
Pavel T <paveltitushkin@gmail.com>
2022-11-05 21:27:56 +0300

    Parsing tabletka.by

--- WebAppMedicalAssistant_Bussines/ServicesImplementations/MedicineService.cs
+++ WebAppMedicalAssistant_Bussines/ServicesImplementations/MedicineService.cs
@@ -1,12 +1,5 @@
 ﻿using AutoMapper;
-using AutoMapper.Configuration.Annotations;
 using HtmlAgilityPack;
-using Microsoft.VisualBasic;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using WebAppMedicalAssistant_Core.Abstractions;
 using WebAppMedicalAssistant_Core.DTO;
 using WebAppMedicalAssistant_Data.Abstractions;
@@ -32,44 +25,19 @@ namespace WebAppMedicalAssistant_Bussines.ServicesImplementations
 
                 var web = new HtmlWeb();
                 var htmlDoc = web.Load(urlSearchSite);
-                var nodes = htmlDoc.DocumentNode.Descendants(0).Where(node => node.HasClass("tooltip-info-header"));
-                
-                var nodeList = new List<string>();
-                var nodeValue = "https://tabletka.by";
-                foreach (var node in nodes)
-                {
-                    var tempNodes = node.ChildNodes
-                        .Select(node => node.GetAttributes("href"));
-                    foreach (var tempNode in tempNodes)
-                    {
-                        foreach (var item in tempNode)
-                        {
-                            if (item != null)
-                            {
-                                if (item.Value.Contains("result"))
-                                {
-                                    nodeValue += item.Value;
-                                    nodeList.Add(nodeValue);
-                                    nodeValue = "https://tabletka.by";
-                                }
-                            }
-                        }
-                    }
-                }
-                var nodeListDistinct = nodeList.Distinct();
 
-                //foreach (var node in nodes)
-                //{
-                //    var tempNode = node.ChildNodes
-                //        .Where(node => node.Name.Equals("a"))
-                //        .Select(node => node.OuterHtml)
-                //        .Aggregate((i, j) => i + Environment.NewLine + j);
-                //    nodeList.Add(tempNode);
-                //}
+                var table = htmlDoc.DocumentNode.Descendants(0)
+                    .Where(node => node.Id == "base-select")
+                    .FirstOrDefault();
+
+                var links = table?.Descendants()
+                    .Where(x => x.Name == "a" && x.GetAttributeValue("href", string.Empty).StartsWith("/result"))
+                    .Select(x => x.GetAttributeValue("href", string.Empty))
+                    .Distinct()
+                    .ToList();
             }
             catch (Exception)
             {
-
                 throw;
             }
         }
